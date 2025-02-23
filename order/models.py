4a5b193b6bd4ca7bda3939a40082a18df0da47fc
EllaCCT <fx374177@gmail.com>
2025-02-23 21:02:06 +0800

    create order app and change order models

---
+++ order/models.py
@@ -0,0 +1,47 @@
+from django.db import models
+from django.conf import settings
+
+class Order(models.Model):
+    PENDING= 'pending'
+    ORDERED = 'ordered'
+    SHIPPED = 'shipped'
+    CANCELLED = 'cancelled'
+    REFUNDED = 'refunded'
+
+    STATUS_CHOICES = (
+        (PENDING, 'Pending'),
+        (ORDERED, 'Ordered'),
+        (SHIPPED, 'Shipped'),
+        (CANCELLED, 'Cancelled'),
+        (REFUNDED, 'Refunded'),
+    )
+    #user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    order_id = models.CharField(max_length=50)
+    product = models.ForeignKey('products.Product', on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    #訂單狀態
+    status = models.CharField(max_length=50, choices=STATUS_CHOICES,default='ordered')
+    
+    # 訂單狀態變更日期
+    shipment_date = models.DateTimeField(null=True, blank=True)
+    cancellation_date = models.DateTimeField(null=True, blank=True)
+    ticket_issue_date = models.DateTimeField(null=True, blank=True)
+    refund_date = models.DateTimeField(null=True, blank=True)
+    
+    class Meta:
+        ordering = ['date_added']
+        
+    def __str__(self):
+        return self.order_id
+    
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order,related_name='items',on_delete=models.CASCADE)
+    product = models.ForeignKey('products.Product', related_name='items',on_delete=models.CASCADE)
+    price=models.IntegerField()
+    quantity=models.IntegerField(default=1)
+
+    def get_total_price(self):
+        return self.price * self.quantity
