ee5b62bf31f97c1f9fd4abf5f6a3b1b7a3db8124
Akiriv <kazysans@gmail.com>
2025-03-20 19:20:44 +0800

    Updated templates and css.

--- cart/urls.py
+++ cart/urls.py
@@ -4,7 +4,7 @@ from cart import views
 
 
 urlpatterns = [
-    path('cart/', cart, name='cart'),
+    path('', cart, name='cart'),
     path('checkout/', checkout, name='checkout'),
     path('add_to_cart/<int:product_id>/', add_to_cart, name='add_to_cart'),
     path('update-cart/<int:product_id>/', views.update_cart, name='update_cart'),
